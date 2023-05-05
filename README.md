78b6158fff97937a78b7f868158a68ef99282b9e
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2023-05-05 19:52:49 +0200

    Setup endpoints to get files based on name / version number

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Using Python 3.8 and Conda.
 
 Run app.py and see responses at: http://127.0.0.1:5000
+
+### Endpoints
+| Endpoint                     | Description                                                          | Example                                                        |
+|------------------------------|----------------------------------------------------------------------|----------------------------------------------------------------|
+| `clean`                      | Removes everything from temp folder and sets up the folder structure | http://127.0.0.1:5000/clean                                    |
+| `/download/<name>/<version>` | Gets trained model from the `model-training` repo                    | http://127.0.0.1:5000/download/c2_Classifier_Sentiment_Model/0 |
+|                              |                                                                      |                                                                |
--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from flask import Flask
+import requests
+import os
+import shutil
 
 app = Flask(__name__)
+github_path_to_trained_models = "https://github.com/remla23-team04/model-training/raw/main/data/trained_models/"
+temp_path_to_trained_models = os.path.join("temp", "trained_models")
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/clean')
+def clean():
+    """
+    Cleans the temp folder and resets the directory tree.
+    :return:
+    """
+    shutil.rmtree("temp")
+    os.mkdir("temp")
+    os.mkdir(temp_path_to_trained_models)
+    return "Temp folder is now empty!"
+
+
+@app.route('/download/<name>/<version>')
+def download(name, version):
+    print(name, version)
+    filename = name + "_" + version
+    temp_file_path = os.path.join(temp_path_to_trained_models, filename)
+    print(temp_file_path)
+    if os.path.isfile(temp_file_path):
+        return "File exists, don't download again!"
+
+    r = requests.get(github_path_to_trained_models + filename)
+    if r.ok:
+        f = open(temp_file_path, 'wb')
+        f.write(r.content)
+        return "Downloaded: " + github_path_to_trained_models + filename
+
+    return "Failed to download: " + github_path_to_trained_models + filename \
+           + "<br> It's probably because there is no trained model with this name / version: " + filename
 
 
 if __name__ == '__main__':

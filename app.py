b7a01f032a8d66b2d25caece3e543b90b7835b84
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2023-05-07 13:26:17 +0200

    Update to download the sentiment model too

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask import request
 import requests
 import os
 import shutil
 
 app = Flask(__name__)
-github_path_to_trained_models = "https://github.com/remla23-team04/model-training/raw/main/data/trained_models/"
+github_path_to_classifier = "https://github.com/remla23-team04/model-training/raw/main/data/trained_models/"    # Trained model
+github_path_to_sentiment_model = "https://github.com/remla23-team04/model-training/blob/main/data/output/"      # .pkl files
 temp_path_to_trained_models = os.path.join("temp", "trained_models")
+temp_path_to_sentiment_models = os.path.join("temp", "sentiment_models")
 
 
-@app.route('/clean')
+@app.route('/clean', methods=['GET'])
 def clean():
     """
     Cleans the temp folder and resets the directory tree.
@@ -17,27 +20,51 @@ def clean():
     shutil.rmtree("temp")
     os.mkdir("temp")
     os.mkdir(temp_path_to_trained_models)
+    os.mkdir(temp_path_to_sentiment_models)
     return "Temp folder is now empty!"
 
 
-@app.route('/download/<name>/<version>')
-def download(name, version):
-    print(name, version)
-    filename = name + "_" + version
-    temp_file_path = os.path.join(temp_path_to_trained_models, filename)
-    print(temp_file_path)
-    if os.path.isfile(temp_file_path):
+@app.route('/download/<classifier_name>/<sentiment_model_name>/<version>', methods=['GET'])
+def download(classifier_name, sentiment_model_name, version):
+    # print(classifier_name, version)
+    classifier_filename = classifier_name + "_" + version
+    classifier_temp_file_path = os.path.join(temp_path_to_trained_models, classifier_filename)
+
+    model_filename = sentiment_model_name + "_" + version
+    model_temp_file_path = os.path.join(temp_path_to_sentiment_models, model_filename) + '.pkl'
+    # print(temp_file_path)
+    if os.path.isfile(classifier_temp_file_path) and os.path.isfile(model_temp_file_path):
         return "File exists, don't download again!"
 
-    r = requests.get(github_path_to_trained_models + filename)
-    if r.ok:
-        f = open(temp_file_path, 'wb')
-        f.write(r.content)
-        return "Downloaded: " + github_path_to_trained_models + filename
+    classifier_req = requests.get(github_path_to_classifier + classifier_filename)
+    model_req = requests.get(github_path_to_sentiment_model + model_filename + '.pkl')
+    if classifier_req.ok and model_req.ok:
+        # Download classifier
+        if classifier_req.ok:
+            f = open(classifier_temp_file_path, 'wb')
+            f.write(classifier_req.content)
 
-    return "Failed to download: " + github_path_to_trained_models + filename \
-           + "<br> It's probably because there is no trained model with this name / version: " + filename
+        # Download sentiment model
+        if model_req.ok:
+            f = open(model_temp_file_path, 'wb')
+            f.write(model_req.content)
 
+        return "Downloaded files!"
+
+    return "Failed to download: " + github_path_to_classifier + classifier_filename \
+           + "<br> It's probably because there is no trained model with this name: " \
+           + classifier_filename + ", version: " + version + \
+           "<br> OR <br> Failed to download: " + github_path_to_sentiment_model + model_filename + '.pkl' \
+           + "<br> It's probably because there is no trained model with this name: " \
+           + model_filename + ", version: " + version
+
+
+@app.route('/submit_review', methods=['POST'])
+def submit_review():
+    data = request.get_json()
+    print(data)
+
+    return data
 
 if __name__ == '__main__':
     app.run()
